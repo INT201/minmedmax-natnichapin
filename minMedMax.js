96a8b27291a68120971dfd7860e456f5ddcfd05a
Natnicha <Natnicha.pin@mail.kmutt.ac.th>
2022-09-02 15:13:16 +0700

    edit1

--- minMedMax.js
+++ minMedMax.js
@@ -1,6 +1,30 @@
 const { template } = require('@babel/core')
 
 function minMedMax(n1, n2, n3) {
-  //code here
+  let object ={Min:undefined,Med:undefined,Max:n1}
+  let arg = [n1,n2,n3]
+
+  for(let i=0 ;i<arg.length;i++){
+      let object ={Min:undefined,Med:undefined,Max:undefined}
+      let arg = [n1,n2,n3]
+      object.Max = arg[0]
+      object.Min = arg[0]
+      for (let i = 0; i < arg.length; i++){
+          if(arg[i]>object.Max){
+              object.Max = arg[i]
+          }
+      }
+      for (let j = 0; j < arg.length; j++){
+          if(arg[j]<object.Min){
+              object.Min = arg[j]
+          }
+      }
+      for (let i = 0; i < arg.length; i++){
+          if(arg[i]!==object.Max && arg[i]!==object.Min){
+              object.Med = arg[i]
+          }
+      }
+      return object
+  }
 }
 module.exports = minMedMax
